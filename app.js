60c8f1c1e88b168024c3493b1a8ff43025952608
Guilherme Cardoso <guilhermecardoso.info@gmail.com>
2024-06-11 13:04:20 -0300

    chore: add new products on base array

--- app.js
+++ app.js
@@ -6,7 +6,15 @@ app.use(express.json());
 
 let products = [
     { id: 1, name: 'Product A', price: 100, imageUrl: 'https://picsum.photos/200/200?random=1' },
-    { id: 2, name: 'Product B', price: 200, imageUrl: 'https://picsum.photos/200/200?random=2' },
+    { id: 2, name: 'Product B', price: 230, imageUrl: 'https://picsum.photos/200/200?random=2' },
+    { id: 3, name: 'Product C', price: 323, imageUrl: 'https://picsum.photos/200/200?random=3' },
+    { id: 4, name: 'Product D', price: 580, imageUrl: 'https://picsum.photos/200/200?random=4' },
+    { id: 5, name: 'Product E', price: 10, imageUrl: 'https://picsum.photos/200/200?random=5' },
+    { id: 6, name: 'Product F', price: 0.70, imageUrl: 'https://picsum.photos/200/200?random=6' },
+    { id: 7, name: 'Product G', price: 99, imageUrl: 'https://picsum.photos/200/200?random=7' },
+    { id: 8, name: 'Product H', price: 777, imageUrl: 'https://picsum.photos/200/200?random=8' },
+    { id: 9, name: 'Product I', price: 900, imageUrl: 'https://picsum.photos/200/200?random=9' },
+    { id: 10, name: 'Product J', price: 1000, imageUrl: 'https://picsum.photos/200/200?random=10' }
 ];
 
 // List all products
